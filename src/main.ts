df16cc82ce458ae59406ff7b300dd9e38c957126
Dyn <dyn.see@gmail.com>
2022-02-04 17:32:26 +0300

    add nest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { moralisTest } from './moralis';
 import { config } from 'dotenv';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 config();
 
-moralisTest();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+
+bootstrap();
